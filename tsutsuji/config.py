c81ae50d478e81b7421cbb34e13e4befd808e264
konawasabi <webmaster@konawasabi.riceball.jp>
2021-08-22 21:52:40 +0900

    test.cfgの指定に従って各軌道の絶対座標を求める

--- tsutsuji/config.py
+++ tsutsuji/config.py
@@ -7,15 +7,32 @@ class Config():
         cp.read(path)
         self.path_parent = pathlib.Path(path).resolve().parent
         sections = cp.sections()
-        track_keys = [i for i in sections if i != '@GENERAL']
+        self.track_keys = [i for i in sections if i != '@GENERAL']
         
-        self.general = {'origin_distance':0, 'offset_variable':'offset'}
+        self.general = {'origin_distance':0, 'offset_variable':'offset', 'resolution':1}
         if '@GENERAL' in sections:
             for k in cp.options('@GENERAL'):
-                self.general[k] = cp['@GENERAL'][k] if k not in ['origin_distance'] else float(cp['@GENERAL'][k])
+                self.general[k] = cp['@GENERAL'][k] if k not in ['origin_distance', 'resolution'] else float(cp['@GENERAL'][k])
             
         self.track_data = {}
-        for tk in track_keys:
-            self.track_data[tk] = {'origin':'absolute','x':0,'y':0,'z':0,'angle':0,'endpoint':0}
+        for tk in self.track_keys:
+            self.track_data[tk] = {'absolute_coordinate':True,\
+                                    'x':0,\
+                                    'y':0,\
+                                    'z':0,\
+                                    'angle':0,\
+                                    'endpoint':0,\
+                                    'file':None,\
+                                    'parent_track':None,\
+                                    'origin_distance':None,\
+                                    'isowntrack':False}
             for k in cp.options(tk):
-                self.track_data[tk][k] = cp[tk][k] if k not in ['x','y','z','angle','endpoint'] else float(cp[tk][k])
+                if k.lower() == 'file':
+                    self.track_data[tk][k] = self.path_parent.joinpath(pathlib.Path(cp[tk][k]))
+                elif k.lower() in ['x','y','z','angle','endpoint','origin_distance']:
+                    self.track_data[tk][k] = float(cp[tk][k])
+                elif k.lower() in ['isowntrack','absolute_coordinate']:
+                    self.track_data[tk][k] = True if cp[tk][k].lower() == 'true' else False
+                else:
+                    self.track_data[tk][k] = cp[tk][k]
+                    
--- tsutsuji/cui.py
+++ tsutsuji/cui.py
@@ -1,11 +1,37 @@
 # python -m tsutsuji.cui で起動
+import matplotlib.pyplot as plt
 from kobushi import mapinterpreter
+from kobushi import trackgenerator
 
 from . import config
 
 conf = config.Config('dev/test.cfg')
+'''
 print(conf.path_parent)
 print(conf.general)
-print(conf.track_data.keys())
-for i in conf.track_data.keys():
+print(conf.track_keys)
+for i in conf.track_keys:
     print(i,conf.track_data[i])
+'''
+
+track = {}
+for i in conf.track_keys:
+    track[i] = {}
+    track[i]['interp'] = mapinterpreter.ParseMap(env=None,parser=None)
+    track[i]['data'] = track[i]['interp'].load_files(conf.track_data[i]['file'])
+    #print(i)
+    #print(track[i]['data'].own_track.data)
+    track[i]['data'].cp_arbdistribution = None
+    track[i]['tgen'] = trackgenerator.TrackGenerator(track[i]['data'],\
+                                                    x0 = conf.track_data[i]['x'],\
+                                                    y0 = conf.track_data[i]['y'],\
+                                                    z0 = conf.track_data[i]['z'],\
+                                                    theta0 = conf.track_data[i]['angle'])
+    track[i]['result'] = track[i]['tgen'].generate_owntrack()
+    #print(track[i]['result'])
+
+for i in conf.track_keys:
+    tmp = track[i]['result']
+    plt.plot(tmp[:,1],tmp[:,2],label=i)
+    plt.legend()
+plt.show()
