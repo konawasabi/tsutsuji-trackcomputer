71d6c551c269f8eb4377d76e6eeefae2c8717425
konawasabi_local <webmaster@konawasabi.riceball.jp>
2023-01-29 14:22:47 +0900

    setup.cfg 依存関係の修正。 クレジット年表記の修正。

--- tsutsuji/gui_tsutsuji.py
+++ tsutsuji/gui_tsutsuji.py
@@ -356,7 +356,7 @@ class mainwindow(ttk.Frame):
     def aboutwindow(self, event=None):
         msg  = 'Tsutsuji trackcomputer\n'
         msg += 'Version '+__version__+'\n\n'
-        msg += 'Copyright © 2022 konawasabi\n'
+        msg += 'Copyright © 2023 konawasabi\n'
         msg += 'Released under the Apache License, Version 2.0 .\n'
         msg += 'https://www.apache.org/licenses/LICENSE-2.0'
         tk.messagebox.showinfo(message=msg)
