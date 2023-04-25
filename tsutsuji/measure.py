d8f2d335f06e2fdf0ed7152d0ac9a7d15cc08a2c
konawasabi_local <webmaster@konawasabi.riceball.jp>
2023-04-25 23:16:34 +0900

    measureウィンドウでの指定modeと内部mode 10, 11が逆転していたのを修正

--- tsutsuji/measure.py
+++ tsutsuji/measure.py
@@ -410,11 +410,11 @@ class interface():
         elif '8-2.' in fitmode:
             result = svIF.mode8(withCpos=False)
         elif '8-3.' in fitmode:
-            result = svIF.mode11()
+            result = svIF.mode10()
         elif '9-1.' in fitmode:
             result = svIF.mode9()
         elif '9-2.' in fitmode:
-            result = svIF.mode10()
+            result = svIF.mode11()
         else:
             raise Exception('invalid fitmode')
         
