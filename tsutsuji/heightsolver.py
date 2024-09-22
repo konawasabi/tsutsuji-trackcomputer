af39e94887fee9d766c1b5045c89a675af3ff9b4
konawasabi_local <webmaster@konawasabi.riceball.jp>
2024-09-22 22:44:07 +0900

    パラメータ入力、モード選択ウィジェットの追加。モード種別は仮名称

--- tsutsuji/heightsolver.py
+++ tsutsuji/heightsolver.py
@@ -43,8 +43,11 @@ class heightSolverUI():
         self.title = ttk.Label(self.parentframe, text='Gradient Solver',font=tk.font.Font(weight='bold'))
         self.title.grid(column=0, row=0, sticky=(tk.E,tk.W))
 
+        self.spacerframe_1 = ttk.Frame(self.parentframe, padding='3 3 3 3')
+        self.spacerframe_1.grid(column=1, row=0, sticky=(tk.E,tk.W))
+
         self.cursorframe = ttk.Frame(self.parentframe, padding='3 3 3 3')
-        self.cursorframe.grid(column=0, row=1, sticky=(tk.E,tk.W))
+        self.cursorframe.grid(column=2, row=0, sticky=(tk.E))
         
         self.cursor_vals = ('α', 'β', 'γ')
         self.cursor_widgets = {}
@@ -54,11 +57,55 @@ class heightSolverUI():
             self.cursor_widgets[key]['label'] = ttk.Label(self.cursorframe, text=key)
             self.cursor_widgets[key]['label'].grid(column=pos*2, row=0, sticky=(tk.E,tk.W))
             self.cursor_widgets[key]['var'] = tk.StringVar()
-            self.cursor_widgets[key]['cb'] = ttk.Combobox(self.cursorframe, textvariable=self.cursor_widgets[key]['var'], width=4)
+            self.cursor_widgets[key]['cb'] = ttk.Combobox(self.cursorframe, textvariable=self.cursor_widgets[key]['var'], width=5)
             self.cursor_widgets[key]['cb'].grid(column=pos*2+1, row=0, sticky=(tk.E,tk.W))
             pos+=1
+            self.cursor_widgets[key]['cb'].state(["readonly"])
         self.make_cursorlist()
-        
+
+        self.paramsframe = ttk.Frame(self.parentframe, padding='3 3 3 3')
+        self.paramsframe.grid(column=0, row=1, sticky=(tk.E,tk.W))
+
+        self.params_vals = ('VCL α', 'VCL β', 'Gr. 1')
+        self.params_widgets = {}
+        pos=0
+        row=0
+        for key in self.params_vals:
+            self.params_widgets[key] = {}
+            self.params_widgets[key]['label'] = ttk.Label(self.paramsframe, text=key)
+            self.params_widgets[key]['label'].grid(column=pos, row=row*2, sticky=(tk.E,tk.W))
+            self.params_widgets[key]['var'] = tk.DoubleVar()
+            self.params_widgets[key]['ent'] = ttk.Entry(self.paramsframe, textvariable=self.params_widgets[key]['var'],width=8)
+            self.params_widgets[key]['ent'].grid(column=pos, row=row*2+1, sticky=(tk.E,tk.W))
+            pos+=1
+            if pos%4 == 0:
+                pos = 0
+                row+=1
+
+        self.spacerframe_2 = ttk.Frame(self.parentframe, padding='3 3 3 3')
+        self.spacerframe_2.grid(column=1, row=1, sticky=(tk.E,tk.W))
+
+        self.modeframe = ttk.Frame(self.parentframe, padding='3 3 3 3')
+        self.modeframe.grid(column=2, row=1, sticky=(tk.E))
+
+        self.fitmode_label = ttk.Label(self.modeframe,text='mode')
+        self.fitmode_label.grid(column=0, row=0, sticky=(tk.E))
+        self.fitmode_list = ('1. α(fix)->β(free)',\
+                             '2. α(free)->β(fix)',\
+                             '3. α(free)->β(free)')
+        self.fitmode_v = tk.StringVar(value=self.fitmode_list[0])
+        self.fitmode_cb = ttk.Combobox(self.modeframe,textvariable=self.fitmode_v,height=len(self.fitmode_list),width=28)
+        self.fitmode_cb.grid(column=1, row=0, sticky=(tk.E))
+        self.fitmode_cb['values'] = self.fitmode_list
+        self.fitmode_cb.state(["readonly"])
+
+        self.doit_b = ttk.Button(self.modeframe, text='Do It',command=None)
+        self.doit_b.grid(column=1, row=1, sticky=(tk.E,tk.W))
+
+        self.mapsyntax_v = tk.BooleanVar(value=True)
+        self.mapsyntax_b = ttk.Checkbutton(self.modeframe, text='mapsyntax', variable=self.mapsyntax_v,onvalue=True,offvalue=False)
+        self.mapsyntax_b.grid(column=0, row=1, sticky=(tk.E))
+            
     def make_cursorlist(self):
         for key in self.cursor_vals:
             #currentval = self.cursor_widgets[key]['var'].get()
