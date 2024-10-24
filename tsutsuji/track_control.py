5b8847b53bc8d2bfab6cd0f0c5ed11a4f22b337b
konawasabi_local <webmaster@konawasabi.riceball.jp>
2024-10-24 23:36:05 +0900

    generate_mapdataメソッドのtrack構文処理をgenerate_tracksyntax/mapstrings化

--- tsutsuji/track_control.py
+++ tsutsuji/track_control.py
@@ -625,78 +625,13 @@ class TrackControl():
         # 他軌道構文生成
         digit_str = '{:.'+'{:d}'.format(self.conf.general['output_digit'])+'f}'
         for tr in [i for i in self.get_trackkeys(self.conf.owntrack) if i not in self.exclude_tracks]:
-            output_map = {'x':'', 'y':'', 'cant':'', 'center':'', 'interpolate_func':'', 'gauge':''}
             if self.conf.general['offset_variable'] is not None:
                 kp_val = '$'+self.conf.general['offset_variable']+' + '
             else:
                 kp_val = ''
-
-            for data in self.rel_track_radius_cp[tr]:
-                if '@' not in tr or '@OT' in tr or (('@KML' in tr or '@CSV' in tr) and self.pointsequence_track.track[tr]['conf']['calc_relrad']):
-                    output_map['x'] += ('{:s}'+digit_str+';\n').format(kp_val,data[0])
-                    output_map['x'] += ('Track[\'{:s}\'].X.Interpolate('+digit_str+','+digit_str+');\n').format(tr,data[3],data[2])
-                    output_map['y'] += ('{:s}'+digit_str+';\n').format(kp_val,data[0])
-                    output_map['y'] += ('Track[\'{:s}\'].Y.Interpolate('+digit_str+','+digit_str+');\n').format(tr,data[6],data[5])
-                else:
-                    output_map['x'] += ('{:s}'+digit_str+';\n').format(kp_val,data[0])
-                    output_map['x'] += ('Track[\'{:s}\'].X.Interpolate('+digit_str+','+digit_str+');\n').format(tr,data[3],0)
-                    output_map['y'] += ('{:s}'+digit_str+';\n').format(kp_val,data[0])
-                    output_map['y'] += ('Track[\'{:s}\'].Y.Interpolate('+digit_str+','+digit_str+');\n').format(tr,data[6],0)
-
-            cp_dist = {}
-            pos_cp = {}
-            relativecp = {}
-            for key in ['cant','interpolate_func','center','gauge']:
-                cp_dist[key], pos_cp[key] = self.takecp(tr,elem=key,supplemental=False)
-                relativecp[key] =  self.convert_relativecp(tr,pos_cp[key])
-
-            if len(relativecp['cant'])>0:
-                '''
-                for data in self.rel_track[tr][np.isin(self.rel_track[tr][:,0],relativecp['cant'][:,0])]:
-                    output_map['cant'] += ('{:s}'+digit_str+';\n').format(kp_val,data[0])
-                    output_map['cant'] += ('Track[\'{:s}\'].Cant.Interpolate('+digit_str+');\n').format(tr,data[8])
-                '''
-                for data in self.convert_cant_with_relativecp(tr,relativecp['cant'][:,3]):
-                    output_map['cant'] += ('{:s}'+digit_str+';\n').format(kp_val,data[0])
-                    output_map['cant'] += ('Track[\'{:s}\'].Cant.Interpolate('+digit_str+');\n').format(tr,data[1])
-
-            
-            key = 'interpolate_func'
-            if len(relativecp[key])>0:
-                for index in range(len(relativecp[key])):
-                    output_map[key] += ('{:s}'+digit_str+';\n').format(kp_val,relativecp[key][index][3])
-                    output_map[key] += ('Track[\'{:s}\'].Cant.SetFunction({:d});\n').format(tr,int(pos_cp[key][index][7]))
-            
-            key = 'center'
-            if len(relativecp[key])>0:
-                for index in range(len(relativecp[key])):
-                    output_map[key] += ('{:s}'+digit_str+';\n').format(kp_val,relativecp[key][index][3])
-                    output_map[key] += ('Track[\'{:s}\'].Cant.SetCenter('+digit_str+');\n').format(tr,pos_cp[key][index][9])
-
-            key = 'gauge'
-            if len(relativecp[key])>0:
-                for index in range(len(relativecp[key])):
-                    output_map[key] += ('{:s}'+digit_str+';\n').format(kp_val,relativecp[key][index][3])
-                    output_map[key] += ('Track[\'{:s}\'].Cant.SetGauge('+digit_str+');\n').format(tr,pos_cp[key][index][10])
-
-            output_file = ''
-            output_file += 'BveTs Map 2.02:utf-8\n\n'
-            # 他軌道構文印字
-            if kp_val != '':
-                output_file += '# offset\n'
-                output_file += ('${:s} = {:f};\n').format(self.conf.general['offset_variable'],self.conf.general['origin_distance'])+'\n'
-            output_file += ('# Track[\'{:s}\'].X\n').format(tr)
-            output_file += output_map['x']+'\n'
-            output_file += ('# Track[\'{:s}\'].Y\n').format(tr)
-            output_file += output_map['y']+'\n'
-            output_file += ('# Track[\'{:s}\'].Cant.Interpolate\n').format(tr)
-            output_file += output_map['cant']+'\n'
-            output_file += ('# Track[\'{:s}\'].Cant.SetFunction\n').format(tr)
-            output_file += output_map['interpolate_func']+'\n'
-            output_file += ('# Track[\'{:s}\'].Cant.SetCenter\n').format(tr)
-            output_file += output_map['center']+'\n'
-            output_file += ('# Track[\'{:s}\'].Cant.SetGauge\n').format(tr)
-            output_file += output_map['gauge']+'\n'
+                
+            output_map = self.generate_tracksyntax(tr,kp_val,digit_str)
+            output_file = self.generate_mapstrings(output_map,tr,kp_val)
 
             if '@' not in tr:
                 self.track[tr]['output_mapfile'] = output_file
